2290870537e4bcc7c81be1be8928a6d7f048fdec
zheerj <Zheer.j@yahoo.com>
2018-09-22 14:54:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ bot.on("ready", async () => {
 
 bot.on("message", async message => {
 
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
-
-  let prefix = '?';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  if (cmd === `${prefix}ping`){
-    const m = await message.channel.send("pong");
+  let prefix = '?'
+  
+  client.on("message", async message => {
+  if(message.author.bot) return;
+  
+    if(message.content.indexOf(config.prefix) !== 0) return;
+  
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  
+    const command = args.shift().toLowerCase();
+  
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
 });
 
